e7bfc630f3f5d2ea62b0ccb22630d7bf580e9b23
arth-abhinav <29733391+arth-abhinav@users.noreply.github.com>
2026-05-12 21:26:11 +0530

    Read events

--- internal/storage/file_tl.go
+++ internal/storage/file_tl.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 )
 
@@ -21,4 +23,68 @@ func (l *FileTransactionLogger) WritePut(key string, value string) {
 
 func (l *FileTransactionLogger) Err() <-chan error {
 	return l.errors
+}
+
+func NewFileTransactionLogger(filename string) (TransactionLogger, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open transaction file: %w", err)
+	}
+
+	return &FileTransactionLogger{file: file}, nil
+}
+
+func (l *FileTransactionLogger) Run() {
+	events := make(chan Event, 16) //replace 16 with env variable
+	errors := make(chan error, 1)
+
+	l.events = events
+	l.errors = errors
+
+	go func() {
+		for e := range l.events {
+			l.lastSequence++
+
+			_, err := fmt.Fprintf(l.file,
+				"%d\t%d\t%s\t%s\n", l.lastSequence, e.EventType, e.Key, e.Value)
+
+			if err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+}
+
+func (l *FileTransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
+	scanner := bufio.NewScanner(l.file)
+	outEvent := make(chan Event)
+	outError := make(chan error, 1)
+
+	go func() {
+		var e Event
+
+		defer close(outEvent)
+		defer close(outError)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			_, err := fmt.Sscanf(line, "%d\t%d\t%s\t%s\n", &e.Sequence, &e.EventType, &e.Key, &e.Value)
+			if err != nil {
+				outError <- fmt.Errorf("input parse error %w", err)
+				return
+			}
+
+			l.lastSequence = e.Sequence
+			outEvent <- e
+		}
+		if err := scanner.Err(); err != nil {
+			outError <- fmt.Errorf("transaction log read failure: %w", err)
+			return
+		}
+
+	}()
+
+	return outEvent, outError
 }
